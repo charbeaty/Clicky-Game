9bfb6ad8fe1f0aed0360b3c210a69cf09c1ea76d
Charissa Beaty <char.beaty11@gmail.com>
2020-01-14 16:41:20 -0700

    fixed import for images.json

--- clicky-game/src/components/MainContainer.js
+++ clicky-game/src/components/MainContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ImageCard from './ImageCard';
-import images from "./images.json";
+import images from '../images.json';
 
 class MainContainer extends Component {
     state = {

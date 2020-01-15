e727df5f63412209b311968a34d6a3f74970025a
Charissa Beaty <char.beaty11@gmail.com>
2020-01-15 12:35:02 -0700

    removed navbar, added image to jumbotron

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
 import Jumbotron from './components/Jumbotron';
 import MainGameContainer from './components/MainContainer'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Jumbotron />
       <MainGameContainer />
     </div>
--- clicky-game/src/components/Jumbotron.js
+++ clicky-game/src/components/Jumbotron.js
@@ -4,6 +4,7 @@ const Jumbotron = () => {
     return (
         <div className="jumbotron jumbotron-fluid">
              <div className="container text-center">
+                 <h1 className="display-4">Remember The Villain!</h1>
                 <p className="lead">Click on an image to earn points! Don't click on an image more than once.</p>
                 <p className="follow">Can you remember the ones you've clicked?</p>
             </div>

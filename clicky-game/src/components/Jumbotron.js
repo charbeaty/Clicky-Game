3cbc4922ae780285cad3d0fa63a20d9575e686f8
Charissa Beaty <char.beaty11@gmail.com>
2020-01-15 10:59:04 -0700

    added navbar and colors

--- clicky-game/src/components/Jumbotron.js
+++ clicky-game/src/components/Jumbotron.js
@@ -4,7 +4,6 @@ const Jumbotron = () => {
     return (
         <div className="jumbotron jumbotron-fluid">
              <div className="container text-center">
-                 <h1 className="display-4">REMEMBER THE VILLAIN!</h1>
                 <p className="lead">Click on an image to earn points! Don't click on an image more than once.</p>
                 <p className="follow">Can you remember the ones you've clicked?</p>
             </div>
---
+++ clicky-game/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+
+        <nav className="navbar">
+           <div className="container text-center">
+                <h1 className="follow">Remember the Villain!</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;

0801c514091ed6523c5c16b3df308bf12fa5cf62
Charissa Beaty <char.beaty11@gmail.com>
2020-01-15 13:11:45 -0700

    Updates

---
+++ precache-manifest.cb2da824db90d898110b08514cdc3922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9d167c79dd2fdce67434d87356c0eb",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4365dce8602019e3421c",
+    "url": "/Clicky-Game/static/css/main.e72c58dd.chunk.css"
+  },
+  {
+    "revision": "f49d31ae12388dff11ba",
+    "url": "/Clicky-Game/static/js/2.fc849696.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.fc849696.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4365dce8602019e3421c",
+    "url": "/Clicky-Game/static/js/main.f6071110.chunk.js"
+  },
+  {
+    "revision": "f2d237b8ad084614ffea",
+    "url": "/Clicky-Game/static/js/runtime-main.e8b6ec8d.js"
+  }
+]);
